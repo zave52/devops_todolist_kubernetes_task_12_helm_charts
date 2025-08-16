be790fba2d188132d2e7d4d141314ad39219bd32
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-16 18:29:48 +0300

    added waiting for ingress controller to be ready

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,12 @@ kubectl taint nodes -l app=mysql app=mysql:NoSchedule
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
 
+# Wait for ingress controller to be ready
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=300s
+
 kubectl apply -f .infrastructure/metricsServer.yml
 
 helm install todoapp-release helm-chart/todoapp
