8e93248ec89099b203c7ae0425a05618705f2142
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-16 17:40:53 +0300

    refactored bootstrap.sh script to use helm for deploying todoapp chart

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-kubectl apply -f .infrastructure/mysql/ns.yml
-kubectl apply -f .infrastructure/mysql/configMap.yml
-kubectl apply -f .infrastructure/mysql/secret.yml
-kubectl apply -f .infrastructure/mysql/service.yml
-kubectl apply -f .infrastructure/mysql/statefulSet.yml
 
-kubectl apply -f .infrastructure/app/ns.yml
-kubectl apply -f .infrastructure/app/pv.yml
-kubectl apply -f .infrastructure/app/pvc.yml
-kubectl apply -f .infrastructure/app/secret.yml
-kubectl apply -f .infrastructure/app/configMap.yml
-kubectl apply -f .infrastructure/app/clusterIp.yml
-kubectl apply -f .infrastructure/app/nodeport.yml
-kubectl apply -f .infrastructure/app/hpa.yml
-kubectl apply -f .infrastructure/app/deployment.yml
+kind create cluster --config cluster.yml
+
+kubectl taint nodes -l app=mysql app=mysql:NoSchedule
+
+helm install todoapp-release helm-chart/todoapp
 
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
