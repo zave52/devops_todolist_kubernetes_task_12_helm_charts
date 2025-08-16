c5ce56b0e5fc6b37dbed305467d7efa7504f3765
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-16 18:17:39 +0300

    refactored bootstrap.sh script, added metricsServer manifest and changed order of installing todoapp chart

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,10 @@ kind create cluster --config cluster.yml
 
 kubectl taint nodes -l app=mysql app=mysql:NoSchedule
 
-helm install todoapp-release helm-chart/todoapp
-
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
+
+kubectl apply -f .infrastructure/metricsServer.yml
+
+helm install todoapp-release helm-chart/todoapp
